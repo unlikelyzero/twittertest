a2327ad9e009116672d047295896f2ee82550e6b
John Hill <jchill2.spam@gmail.com>
2016-11-28 21:20:58 -0500

    Update test_twitterAPI_login.py

--- test_twitterAPI_login.py
+++ test_twitterAPI_login.py
@@ -1,15 +1,16 @@
 #pip install requests_oauthlib
 #pip install pytest
 import pytest
+import os
 from requests_oauthlib import OAuth1Session
 
 consumer_key = os.environ.get('CONSUMER_KEY')
 consumer_key = os.environ.get('CONSUMER_SECRET')
 access_token_key = os.environ.get('ACCESS_TOKEN_KEY')
-access_token_secret_key = os.environ.get('ACCESS_TOKEN_SECRET_KEY')
+access_token_secret = os.environ.get('ACCESS_TOKEN_SECRET')
 
 def test_login_to_twitter():
     url = 'https://api.twitter.com/1.1/account/verify_credentials.json'
-    twitter = OAuth1Session(consumer_key, consumer_key, access_token_key, access_token_secret_key)
+    twitter = OAuth1Session(consumer_key, consumer_key, access_token_key, access_token_secret)
     r = twitter.get(url)
     assert r.status_code == 200
