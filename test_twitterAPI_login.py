97e45d1bd404413095682d0973771e4957a35da5
John Hill <jchill2.spam@gmail.com>
2016-11-28 21:48:54 -0500

    Adding negative test

--- test_twitterAPI_login.py
+++ test_twitterAPI_login.py
@@ -19,3 +19,9 @@ def test_login_to_twitter():
     twitter = OAuth1Session(consumer_key, consumer_secret, access_token_key, access_token_secret)
     r = twitter.get(url)
     assert r.status_code == 200
+
+def test_login_to_twitter_negative():
+    url = 'https://api.twitter.com/1.1/account/verify_credentials.json'
+    twitter = OAuth1Session(consumer_key, consumer_secret, access_token_key, 'asdf')
+    r = twitter.get(url)
+    assert r.status_code == 401
